5c3420126c50dfee535c635bf735071537555d3e
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-07-03 20:04:20 +0530

    feat: #10

---
+++ url-shortener-web/src/components/Card.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Stack, Typography } from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import SettingsIcon from '@mui/icons-material/Settings';
+import KeyIcon from '@mui/icons-material/Key';
+import Proptypes from 'prop-types';
+
+const Card = (props) => {
+  const { key, title, description, iconName, style, onClick } = props;
+  return (
+    <Stack key={key} p={3} sx={ style } width={280} height={300} onClick={onClick}>
+      { iconName === 'link' && <LinkIcon fontSize='large' /> }
+      { iconName === 'key' && <KeyIcon fontSize='large' /> }
+      { iconName === 'gear' && <SettingsIcon fontSize='large' /> }
+      <Typography variant='h4' mt={1.5}>{title}</Typography>
+      <Typography variant='p' mt={1}>{description}</Typography>
+    </Stack>
+  );
+};
+
+Card.propTypes = {
+  key: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  description: Proptypes.string.isRequired,
+  iconName: Proptypes.string.isRequired,
+  style: Proptypes.object,
+  onClick: Proptypes.func
+};
+
+export default Card;
